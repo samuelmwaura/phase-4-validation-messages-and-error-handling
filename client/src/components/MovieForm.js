63d2604db8bce27a53e6b13967b45334ecd99ffc
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-10-03 19:54:56 +0300

    Completed the code along

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import styled from "styled-components";
 
 function MovieForm() {
+  const [errors,setErrors] = useState([])
   const [formData, setFormData] = useState({
     title: "",
     year: new Date().getFullYear(),
@@ -14,6 +15,7 @@ function MovieForm() {
     female_director: false,
   });
 
+ 
   function handleSubmit(e) {
     e.preventDefault();
     fetch("/movies", {
@@ -23,9 +25,36 @@ function MovieForm() {
       },
       body: JSON.stringify(formData),
     })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
+      .then((response) => {
+        if(response.ok){
+          response.json().then(newMovie =>console.log(newMovie))
+        }else{
+          response.json().then(errorData =>setErrors(errorData.errors)) //errorData.errors is an array since it was formatted in the bankend using rails fullmessages
+        }
+
+      })
+      // .then((newMovie) => console.log(newMovie));
   }
+
+//   // make the function async to enable the await keyword
+// async function handleSubmit(e) {
+//   e.preventDefault();
+//   // fetch returns a Promise, we must await it
+//   const response = await fetch("/movies", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(formData),
+//   });
+//   // response.json() returns a Promise, we must await it
+//   const data = await response.json();
+//   if (response.ok) {
+//     console.log("Movie created:", data);
+//   } else {
+//     setErrors(data.errors);
+//   }
+// }
 
   function handleChange(e) {
     const value =
@@ -117,7 +146,7 @@ function MovieForm() {
         <FormGroup>
           <label htmlFor="female_director">
             Female Director?
-            <input
+            <input  
               type="checkbox"
               id="female_director"
               checked={formData.female_director}
@@ -125,6 +154,15 @@ function MovieForm() {
             />
           </label>
         </FormGroup>
+
+       
+  {errors.length > 0 && (
+    <ul style={{ color: "red" }}>
+      {errors.map((error) => (
+        <li key={error}>{error}</li>
+      ))}
+    </ul>
+  )}
         <SubmitButton type="submit">Add Movie</SubmitButton>
       </form>
     </Wrapper>
